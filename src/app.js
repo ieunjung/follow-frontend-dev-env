73aa34ac71cac19ac8764ea24d4c875058571068
ieunjung <euju718@gmail.com>
2021-02-03 18:32:39 +0900

    자주 사용하는 플러그인

--- src/app.js
+++ src/app.js
@@ -6,3 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <img src="${nyancat}"/>
     `
 })
+
+console.log(process.env.NODE_ENV);
+console.log(TWO);
+console.log(TWO_STR);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require("path");
 const MyWebpackPlugin = require("./my-webpack-plugin");
 const webpack = require("webpack");
 const childProcess = require("child_process");
+const { config } = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // 이것과 같음! 
 // node_modules/.bin/webpack --mode development --entry ./src/app.js -o dist
@@ -28,15 +32,18 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    // process.env.NODE_ENV === "production"
+                    //     ? MiniCssExtractPlugin.loader // 프로덕션 환경
+                    //     : 
+                        "style-loader", // 개발 환경
+                    "css-loader"
                 ]
             },
             {
                 test: /\.(png|jpg)$/,
                 loader: 'url-loader',
                 options: {
-                    publicPath: './dist/',
+                    // publicPath: './dist/',
                     name: '[name].[ext]?[hash]',
                     limit: 20000, // 2kb 이상인 경우는 file-loader 가 실행됨.
                 }
@@ -47,10 +54,31 @@ module.exports = {
         // new MyWebpackPlugin(),
         new webpack.BannerPlugin({
             banner: `
-                Build Date : ${new Date().toLocaleDateString()}
-                Commit Version : ${childProcess.execSync("git -rev-parse --short HEAD")}
+                Build Date : ${new Date().toLocaleString()}
+                Commit Version : ${childProcess.execSync("git rev-parse --short HEAD")}
+                Author : ${childProcess.execSync("git config user.name")}
             `
-        })
+        }),
+        new webpack.DefinePlugin({
+            TWO: "1+1",
+            TWO_STR: JSON.stringify("1+1"),
+            'api.domain': JSON.stringify("https://github.com")
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            templateParameters: {
+                env: process.env.NODE_ENV === "development" ? ("개발용") : ""
+            },
+            minify: process.env.NODE_ENV === "production" ? {
+                collapseWhitespace: true,
+                removeComments: true
+            } : false
+        }),
+        new CleanWebpackPlugin(),
+        // ...(process.env.NODE_ENV === "production"
+        //     ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+        //     : []
+        //     )
     ]
 }
 
